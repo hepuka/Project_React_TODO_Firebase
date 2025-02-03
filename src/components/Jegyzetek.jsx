77db41cdecdd314a5f72b65e5be6984bda34be09
kun-fagyal_zoltan <kunzoltan7707@gmail.com>
2025-02-03 20:21:23 +0100

    upd

--- src/components/Jegyzetek.jsx
+++ src/components/Jegyzetek.jsx
@@ -74,11 +74,11 @@ const Jegyzetek = () => {
           onChange={(e) => setDesc(e.target.value)}
         ></textarea>
 
-        <button className="mt-4 text-xs bg-transparent dark:hover:bg-gray-700 text-gray-700 dark:text-white font-bold py-2 px-4 border border-gray-400 rounded shadow">
+        <button className="mt-4 w-full text-xs bg-transparent dark:hover:bg-gray-700 text-gray-700 dark:text-white font-bold py-2 px-4 border border-gray-400 rounded shadow">
           Hozzáad
         </button>
       </form>
-      <div className=" w-full mt-4 md:w-96 sm:w-96 bg-transparent ">
+      <div className=" w-full mt-10 md:w-96 sm:w-96 bg-transparent ">
         {notes.map((note) => (
           <div
             className="flex justify-between mb-5 p-2.5 w-full md:w-96 sm:w-96 text-sm text-gray-900  rounded-lg border border-gray-300 dark:bg-gray-800 dark:border-gray-600 dark:text-white"
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,19 +2,6 @@ import React from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { Link } from "react-router-dom";
 
-/*           <div
-            className="flex justify-between mb-5 p-2.5 w-full md:w-96 sm:w-96 text-sm text-gray-900 rounded-lg border border-gray-300  dark:border-gray-600 dark:bg-gray-800  dark:text-white"
-            key={note.id}
-          >
-            <div className="flex justify-between ">
-              <div className="flex flex-col gap-2">
-                <p className="text-sm font-bold">{note.title}</p>
-                <p className="text-xs dark:text-gray-300">{note.date}</p>
-                <p className="text-sm dark:text-gray-200">{note.desc}</p>
-              </div>
-            </div>
-          </div> */
-
 export default function Note({ todo, handleDelete }) {
   return (
     <div className="flex w-full min-h-24 justify-between">
